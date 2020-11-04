fe50751fee87755c54e29ee2a3d0ad7516598085
Tran Quoc Toan <70750334+toantq511@users.noreply.github.com>
2020-11-04 17:01:36 +0700

    Updates

---
+++ precache-manifest.fc6ef6941884f3b97e627ab1f27f940b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2cb6b1a3ad13515db44002bab0fbad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe129a4c298d43e7f745",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "08fb069d398a690d9cb3",
+    "url": "/static/css/main.3013197c.chunk.css"
+  },
+  {
+    "revision": "fe129a4c298d43e7f745",
+    "url": "/static/js/2.fc1401d9.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/static/js/2.fc1401d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08fb069d398a690d9cb3",
+    "url": "/static/js/main.13a9fef6.chunk.js"
+  },
+  {
+    "revision": "39815d60ea65f48d87c0",
+    "url": "/static/js/runtime-main.fd55cd72.js"
+  }
+]);
