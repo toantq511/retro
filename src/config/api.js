a399d06d1c7f1197b57f8081ece97a97ef8e82ac
Tran Quoc Toan <quoctoan.tran@citynow.vn>
2020-11-04 19:02:05 +0700

    fix api

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,4 @@
-export const api = "http://localhost:8080/api";
+export const api =
+	process.env.NODE_ENV === "development"
+		? "http://localhost:8080/api"
+		: "https://protected-tor-34893.herokuapp.com/api";
