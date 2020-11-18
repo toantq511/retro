a829d5f3a3b0d50009550e535811a059a666548c
Tran Quoc Toan <70750334+toantq511@users.noreply.github.com>
2020-11-18 12:50:43 +0700

    env

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,1 @@
-// require("dotenv").config();
-// console.log(process.env);
-// export const api = "https://protected-tor-34893.herokuapp.com/api";
-export const api = "http://localhost:8080/api";
+export const api = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
